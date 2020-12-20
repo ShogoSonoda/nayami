493a45a1baef041d846a8cc83039126e8f397b59
sonoda <sonoponsyougo@gmail.com>
2020-12-20 10:27:40 +0900

    悩み編集機能

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,4 +1,6 @@
 class ProblemsController < ApplicationController
+  before_action :set_problem, only: [:edit, :update]
+
   def index
     @problems = Problem.includes(:user).order("created_at DESC")
   end
@@ -16,8 +18,24 @@ class ProblemsController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @problem.update(problem_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def problem_params
     params.require(:problem).permit(:title, :text).merge(user_id: current_user.id)
+  end
+
+  def set_problem
+    @problem = Problem.find(params[:id])
   end
 end
