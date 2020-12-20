0391be673b7c9a5a300ceae61bd8f30e3eb9e18f
sonoda <sonoponsyougo@gmail.com>
2020-12-20 09:33:16 +0900

    投稿機能

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,22 @@
 class ProblemsController < ApplicationController
   def index
-    
+  end
+
+  def new
+    @problem = Problem.new
+  end
+
+  def create
+    @problem = Problem.new(problem_params)
+    if @problem.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def problem_params
+    params.require(:problem).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
