f2715f7a7d91bae6ad1dd5183bb129c4b9543efb
sonoda <sonoponsyougo@gmail.com>
2020-12-21 22:03:40 +0900

    コントローラーの作成

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,14 @@
+class AnswersController < ApplicationController
+  def create
+    if answer = Answer.create(answer_params)
+      redirect_to problem_path(params[:problem_id])
+    else
+      render problem_path(params[:problem_id])
+    end
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:text).merge(user_id: current_user.id, problem_id: params[:problem_id])
+  end
+end
