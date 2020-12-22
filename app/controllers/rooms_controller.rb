b442f7b43bcef3a4f97c66eb4d36b1705d38c954
sonoda <sonoponsyougo@gmail.com>
2020-12-22 22:55:58 +0900

    ルームの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,23 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    @room.user_ids = current_user.id
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
