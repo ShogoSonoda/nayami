ceaf644713a16896149c31af98b30b014cb74956
sonoda <sonoponsyougo@gmail.com>
2020-12-24 23:16:30 +0900

    テストコード

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @room = FactoryBot.build(:room)
+  end
+
+  describe 'ルーム新規作成' do
+    it "nameが存在しないと作成できないこと" do
+      expect(@room).to be_valid
+    end
+  end
 end
