781a4790795902811fd713573ccdd1debff4d568
sonoda <sonoponsyougo@gmail.com>
2020-12-26 08:02:38 +0900

    コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'messages/index'
-  get 'messages/create'
   root to: 'problems#index'
+
   devise_for :users
+
   resources :problems do
     resources :answers, only: [:create]
   end
+
   resources :rooms, only: [:index, :show, :new, :create] do
     resources :messages, only: [:index, :create]
+  end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
   end
 end
