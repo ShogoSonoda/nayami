26692cb2ba45e2f9147f5a8023998b31d6497188
sonoda <sonoponsyougo@gmail.com>
2020-12-28 21:45:32 +0900

    Active Storageの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
     @rooms = @user.rooms
   end
 
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    if @profile.update()
+      
+    end
+  end
+
   def follows
     @user = User.find(params[:id])
     @users = @user.followings
@@ -16,5 +26,10 @@ class UsersController < ApplicationController
   def followers
     @user = User.find(params[:id])
     @users = @user.followers
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:project, :occupation, :position, :introduction).merge(user_id: current_user.id)
   end
 end
