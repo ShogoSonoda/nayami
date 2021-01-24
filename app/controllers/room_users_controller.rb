37c7444d100ad5de873f9edbda5baf2df3a7c8d0
sonopon <sonoponsyougo@gmail.com>
2021-01-24 13:09:04 +0900

    redirect_back(fallback_location: root_path)の導入
    現在のページを更新するため

--- app/controllers/room_users_controller.rb
+++ app/controllers/room_users_controller.rb
@@ -2,12 +2,12 @@ class RoomUsersController < ApplicationController
   def create
     room_user = current_user.room_users.build(room_id: params[:room_id])
     room_user.save
-    redirect_to room_path(params[:room_id])
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     room_user = RoomUser.find_by(room_id: params[:room_id], user_id: current_user.id)
     room_user.destroy
-    redirect_to rooms_path
+    redirect_back(fallback_location: root_path)
   end
 end
