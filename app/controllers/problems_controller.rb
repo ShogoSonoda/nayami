9f50eed02d8e1a6ae35fe4ebbf89bca7ce175ab0
sonopon <sonoponsyougo@gmail.com>
2021-02-08 22:54:58 +0900

    ゲストユーザーのnameを設定

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -5,6 +5,7 @@ class ProblemsController < ApplicationController
 
   def new_guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲストユーザー"
       user.password = SecureRandom.urlsafe_base64
     end
     sign_in user
