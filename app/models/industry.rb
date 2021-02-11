c18758eadb6044944da836e9a59980314eec8d7b
sonopon <sonoponsyougo@gmail.com>
2021-02-11 18:45:36 +0900

    id:1の削除

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,15 +1,14 @@
 class Industry < ActiveHash::Base
   self.data = [
-    # {id: 1, name: '--'},
-    { id: 2, name: 'メーカー' },
-    { id: 3, name: 'サービス・インフラ' },
-    { id: 4, name: '商社(総合・専門)' },
-    { id: 5, name: '銀行・証券・保険・金融' },
-    { id: 6, name: '情報(広告・通信・マスコミ)' },
-    { id: 7, name: '百貨店・専門店・流通・小売' },
-    { id: 8, name: 'IT・ソフトウェア・情報処理' },
-    { id: 9, name: '公務員' },
-    { id: 10, name: 'その他' }
+    { id: 1, name: 'メーカー' },
+    { id: 2, name: 'サービス・インフラ' },
+    { id: 3, name: '商社(総合・専門)' },
+    { id: 4, name: '銀行・証券・保険・金融' },
+    { id: 5, name: '情報(広告・通信・マスコミ)' },
+    { id: 6, name: '百貨店・専門店・流通・小売' },
+    { id: 7, name: 'IT・ソフトウェア・情報処理' },
+    { id: 8, name: '公務員' },
+    { id: 9, name: 'その他' }
   ]
 
   include ActiveHash::Associations
